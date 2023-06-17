5add52ab3774d9994571afddf0c98fc45aeac5bf
SHIN96BC <mousecjf@gmail.com>
2023-06-17 13:15:24 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/app.js'
+    },
+    output: {
+        path: path.resolve('./dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 10 * 1024 // 10kb
+                    }
+                }
+            }
+        ]
+    }
+};
