d96e8eaffa62908a9a216126f459ce478f2be306
SHIN96BC <mousecjf@gmail.com>
2023-07-03 21:56:43 +0900

    babel 프리셋 강의 진행중

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    presets: [
+        ['@babel/preset-env', {
+            targets: {
+                chrome: '79', // 크롬 79까지 지원하는 코드를 만듭니다.
+            }
+        }]
+    ]
+};
