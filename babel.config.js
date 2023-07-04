cb074516731fe2aca70d39fb3f7bcb99588ed1eb
SHIN96BC <mousecjf@gmail.com>
2023-07-04 22:22:15 +0900

    env 프리셋 설정과 폴리필

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,11 @@ module.exports = {
         ['@babel/preset-env', {
             targets: {
                 chrome: '79', // 크롬 79까지 지원하는 코드를 만듭니다.
+                ie: '11',     // 인터넷 익스플로러 11까지 지원하는 코드를 만듭니다.
+            },
+            useBuiltIns: 'usage',  // 기본값이 usage 입니다.('entry', false도 올 수 있습니다.)
+            corejs: {
+                version: 2, // 기본값이 2입니다. (3도 올 수 있습니다.) 
             }
         }]
     ]
